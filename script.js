095f756c273835ce4541836dc5547358985c4b0b
Brayden Curtis <braydencurtis@gmail.com>
2020-03-13 17:35:59 -0700

    Turned answer randomizer into a method for the magic8Ball object

--- script.js
+++ script.js
@@ -5,26 +5,24 @@ $(document).ready(function(){
         listOfAnswers: ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
     };
 
+    magic8Ball.askQuestion = function(question){
+        var random = Math.random();
+        var randomArray = random * magic8Ball.listOfAnswers.length;
+        var randomIndex = Math.floor(randomArray);
+        var answer = this.listOfAnswers[randomIndex];
+
+        $("#answer").text(answer);
+
+        console.log(question);
+        console.log(answer);
+    };
     
 
     // console.log(randomAnswer);
 
     var onClick = function() {
         var question = prompt("ASK A YES/NO QUESTION!");
-        magic8Ball.askQuestion = question;
-        var random = Math.random();
-
-        var randomArray = random * magic8Ball.listOfAnswers.length;
-
-        var randomIndex = Math.floor(randomArray);
-
-        var randomAnswer = magic8Ball.listOfAnswers[randomIndex];
-
-
-
-
-        $("#answer").text(randomAnswer);
-
+        magic8Ball.askQuestion(question);
     };
 
 
