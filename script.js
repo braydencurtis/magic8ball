2d9930c177770c1bdb52f75523247d304ca2a2a5
Brayden Curtis <braydencurtis@gmail.com>
2020-03-13 19:26:24 -0700

    Change 8ball image depending on if question or answer mode

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 $(document).ready(function(){
-	
+	// Hide default answer on page load
 	$("#answer").hide();
 	
     var magic8Ball = {
         listOfAnswers: ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
     };
 
+
+// Randomly choose answer from array then display it on page 
     magic8Ball.askQuestion = function(question){
+        // Randomly choose answer then assign to answer variable
         var random = Math.random();
         var randomArray = random * magic8Ball.listOfAnswers.length;
         var randomIndex = Math.floor(randomArray);
         var answer = this.listOfAnswers[randomIndex];
 
+        //Change displayed answer to randomly chosen answer from array
         $("#answer").text(answer);
+        //Switch ball image to answer side
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        // Fade in answer over 4 seconds
         $("#answer").fadeIn(4000);
 
+        // Log question and answer to console
         console.log(question);
         console.log(answer);
     };
     
-
-    // console.log(randomAnswer);
-
+    // Prompt user for new question then run answer function
     var onClick = function() {
+        // Hide answer on screen
         $("#answer").hide();
-		var question = prompt("ASK A YES/NO QUESTION!");
-        magic8Ball.askQuestion(question);
+        //Switch ball image to question side
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+
+        // Delay prompt so image has time to change
+        setTimeout(function(){
+            var question = prompt("ASK A YES/NO QUESTION!");
+            magic8Ball.askQuestion(question);}
+        , 500);
     };
 
 
-
+    // Run question prompt on button click
     $("#questionButton").click(onClick);
 
 
