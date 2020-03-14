174db96113df1f7f1f8e89e46676300be8613fce
Brayden Curtis <braydencurtis@gmail.com>
2020-03-13 18:57:31 -0700

    Hide answer upon page load and when starting a new question

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-
-
+	
+	$("#answer").hide();
+	
     var magic8Ball = {
         listOfAnswers: ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
     };
@@ -21,7 +22,8 @@ $(document).ready(function(){
     // console.log(randomAnswer);
 
     var onClick = function() {
-        var question = prompt("ASK A YES/NO QUESTION!");
+        $("#answer").hide();
+		var question = prompt("ASK A YES/NO QUESTION!");
         magic8Ball.askQuestion(question);
     };
 
